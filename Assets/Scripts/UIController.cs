2be1cfa30db2f2397efeabdc2253fa2e74b88808
Vinicius Barbosa <vininew921@hotmail.com>
2021-11-12 22:40:01 -0300

    Improved leaderboards performance

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -23,7 +23,9 @@ public class UIController : MonoBehaviour
     public void ShowLeaderboards(int score, int level)
     {
         string scoreString = score.ToString() + " - " + level.ToString();
-        GameJolt.API.Scores.Add(score, scoreString, nicknameInput.text);
-        GameJoltUI.Instance.ShowLeaderboards();
+        GameJolt.API.Scores.Add(score, scoreString, nicknameInput.text, callback: (bool success) =>
+        {
+            GameJoltUI.Instance.ShowLeaderboards();
+        });
     }
 }
